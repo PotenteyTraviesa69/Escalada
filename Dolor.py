77f36f8af4f81d59642f2596028ba3dc5050621e
PotenteyTraviesa69 <sirjordi87@gmail.com>
2026-01-29 12:18:31 +0100

    Update Dolor.py

--- Dolor.py
+++ Dolor.py
@@ -234,11 +234,11 @@ def draw_pattern_in_region(draw_ctx, polygon, user_pattern, color_rgb):
     
     elif user_pattern == "lines_oblique":
         line_color = color_rgb + (255,) 
-        for x in range(min_x, max_x, 1):
-            for y in range(min_y, max_y, 1):
+        for x in range(min_x, max_x, 3):
+            for y in range(min_y, max_y, 3):
                 if (x + y) % 10 == 0: 
                     if point_in_polygon(x, y, polygon):
-                        draw_ctx.line((x, y, x+4, y+4), fill=line_color, width=1)
+                        draw_ctx.line((x, y, x+4, y+4), fill=line_color, width=2)
 
     elif user_pattern == "cross":
         cross_color = color_rgb + (255,)
